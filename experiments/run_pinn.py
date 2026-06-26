71dd4f4491d4f4afbbacb344824153af86c183de
Zhuxinchen-code-learning <h2749144740@gmail.com>
2026-06-26 10:58:49 +0800

    Add PINN experiment placeholder

---
+++ experiments/run_pinn.py
@@ -0,0 +1,9 @@
+"""Placeholder entry point for the PINN training experiment."""
+
+
+def main() -> None:
+    raise NotImplementedError("PINN training will be implemented in the next stage.")
+
+
+if __name__ == "__main__":
+    main()
